8557ba25c87460b9ba4158f2837735e5ebb46a02
T. Cameron Waller <tcameronwaller@gmail.com>
2022-07-29 10:08:00 -0500

    version check

--- package/mcita_organization.py
+++ package/mcita_organization.py
@@ -719,7 +719,7 @@ def execute_procedure(
     # Report version.
     utility.print_terminal_partition(level=1)
     print(path_dock)
-    print("version check: 1")
+    print("version check: 2")
     # Pause procedure.
     time.sleep(5.0)
 
